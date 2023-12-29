01b00a332b85f7f4e1a16bf17131968f0b54f5f9
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2023-12-29 15:55:22 -0800

    Initial Commit

---
+++ Invasives-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Invasives-iOS
+//
+//  Created by Matthew Logan on 2023-12-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Invasives-iOS/Invasives_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Invasives_iOSApp.swift
+//  Invasives-iOS
+//
+//  Created by Matthew Logan on 2023-12-29.
+//
+
+import SwiftUI
+
+@main
+struct Invasives_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
